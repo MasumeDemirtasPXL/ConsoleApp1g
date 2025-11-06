84542749ee1065cf4068a61cacd2b94cbb49723e
Masume Demirtas <12502779@student.pxl.be>
2025-11-06 08:28:58 +0100

    Afgewerkt

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geen stress, alles komt goed. Geloof in jezelf <3");
+        }
+    }
+}
